6a5c1bc73727167ecf990378880169bbf7f75f7d
tayyjohnson <taydriver1@gmail.com>
2022-10-15 17:16:53 -0400

    styling issues

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,11 +1,11 @@
 import { Workbox } from "workbox-window";
 import Editor from "./editor";
 import "./database.js";
-import "./client/src/css/style.css";
+import "./css/style.css";
 import Logo from "./images/logo.png";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { initDb, getDb, postDb } from "./database";
+import { initDb, getDb, postDb } from "./database.js";
 
 window.addEventListener("load", function () {
   initDb();
